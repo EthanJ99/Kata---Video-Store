f5aa4c3bb8af4f0d14c72fdcf2622ce751231c8e
EthanJ99 <9293417+EthanJ99@users.noreply.github.com>
2025-12-04 21:23:25 +0000

    Implement new release rentals

--- src/generate_statement.py
+++ src/generate_statement.py
@@ -1,21 +1,28 @@
+
+
 def generate_statement(rental_data, customer_name):
     # Get price per rental
     # Get Total price of all
     # Get number of frequent renter points
     statement = ""
+
     total_price = 0.0
     renter_points = 0
 
-    statement += f"Rental Record for {customer_name}\n"
-
+    statement += f"Rental Record for Customer {customer_name}\n"
+    
     for rental in rental_data:
-        price = 2.0 if rental["length"] <= 2 else (rental["length"] - 2) * 1.50 + 2.0
+        if(rental["type"] == "regular"):
+            price = 2.0 if rental["length"] <= 2 else (rental["length"] - 2) * 1.50 + 2.0
+            renter_points += 1
+        else:
+            price = rental["length"] * 3.0
+            renter_points += 2 if rental["length"] >= 2 else 1
 
         total_price += price
-        renter_points += 1
-
+        
         statement += f"{rental['name']}  £{price}\n"
-
+    
     statement += f"You owe £{total_price}\n"
     statement += f"You earned {renter_points} frequent renter points"
     
--- test/test_create_statement.py
+++ test/test_create_statement.py
@@ -1,4 +1,4 @@
-from src.generate_statement import VideoStore, generate_statement
+from src.generate_statement import generate_statement
 
 
 def test_generate_statement_for_three_regular_rentals():
@@ -17,5 +17,37 @@ def test_generate_statement_for_three_regular_rentals():
         "You earned 3 frequent renter points"
     )
 
-    assert VideoStore(rental_data, "Customer Name").generate_statement() == expected_statement
+    assert generate_statement(rental_data=rental_data, customer_name="Name") == expected_statement
+
+def test_generate_statement_for_one_new():
+    rental_data = [
+        {"name": "Crazynotes", "length": 1, "type": "new"}
+    ]
+
+    expected_statement = (
+        "Rental Record for Customer Name\n"
+        "Crazynotes  £3.0\n"
+        "You owe £3.0\n"
+        "You earned 1 frequent renter points"
+    )
+
+    assert generate_statement(rental_data=rental_data, customer_name="Name") == expected_statement
+
+def test_generate_statement_for_three_new_rentals():
+    rental_data = [
+        {"name": "Crazynotes", "length": 1, "type": "new"},
+        {"name": "Teeth", "length": 2, "type": "new"},
+        {"name": "The Web", "length": 3, "type": "new"},
+    ]
+
+    expected_statement = (
+        "Rental Record for Customer Name\n"
+        "Crazynotes  £3.0\n"
+        "Teeth  £6.0\n"
+        "The Web  £9.0\n"
+        "You owe £18.0\n"
+        "You earned 5 frequent renter points"
+    )
+
+    assert generate_statement(rental_data=rental_data, customer_name="Name") == expected_statement
 
